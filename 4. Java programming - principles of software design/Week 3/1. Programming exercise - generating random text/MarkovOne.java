01bd1b50da433c5a13c2265280470462407bfdd0
Brienna Herold <brienna.herold@gmail.com>
2017-01-07 18:25:59 -0500

    Clarify some things & set break condition in MarkovOne.getRandomText()

--- 4. Java programming - principles of software design/Week 3/1. Programming exercise - generating random text/MarkovOne.java
+++ 4. Java programming - principles of software design/Week 3/1. Programming exercise - generating random text/MarkovOne.java
@@ -30,15 +30,20 @@ public class MarkovOne {
 			return "";
 		}
 		StringBuilder sb = new StringBuilder();
-		// Generate a random index between 0 and size of myText minus 1 (for MarkovOne)
+		// Generate a random index (from valid indexes, which are all those indexes that have 
+		// following characters. The last index has no following character, therefore
+		// myText.length() - 1 is used
 		int index = myRandom.nextInt(myText.length() - 1);
-		// Retrieve a substring that is 
+		// Assign to key the one-character string at the random index
 		String key = myText.substring(index, index + 1);
 		sb.append(key);
-		// ABOVE THIS DON'T UNDERSTAND YET
 		for(int k=0; k < numChars; k++){
 		    // Find all characters that follow the current character
 		    ArrayList<String> follows = getFollows(key);
+		    // Break if no characters were found
+		    if (follows.size() == 0) {
+		        break;
+		    }
 		    // Randomly pick one of them as the successor
 		    index = myRandom.nextInt(follows.size());
 		    String successor = follows.get(index);
